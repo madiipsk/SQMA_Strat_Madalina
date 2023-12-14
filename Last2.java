6784c0526ffc7c112444d20f26e53b55e2453de7
madiipsk <91597690+madiipsk@users.noreply.github.com>
2023-12-14 22:41:38 +0200

    Create Last2.java

---
+++ Last2.java
@@ -0,0 +1,31 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MatematicaTest {
+
+    public Matematica mate;
+    @Before
+    public void setUp(){
+        mate = new Matematica();
+    }
+
+    @Test
+    public void testEsteParCorect(){
+        assertTrue(mate.estePar(6));
+        assertTrue(mate.estePar(18));
+        assertTrue(mate.estePar(78));
+        assertFalse(mate.estePar(5));
+        assertFalse(mate.estePar(89));
+    }
+
+    @Test
+    public void testNrPareListNotNull(){
+        List<Integer> lista=mate.nNumerePare(2);
+        assertNotNull(lista);
+    }
+}
