56cf22e922cad647f72110d2269904a2e3b21bbe
madiipsk <91597690+madiipsk@users.noreply.github.com>
2023-12-14 22:40:52 +0200

    Create Second2.java

---
+++ Second2.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MatematicaTest {
+    public Matematica mate;
+    @Before
+    public void setUp(){
+        mate = new Matematica();
+    }
+
+    @Test
+    public void testRaportZecimalCorect(){
+
+        double rezultat= mate.raport(27,6);
+        assertEquals(4.5, rezultat,0.001);
+    }
+
+    @Test
+    public void testRaportShouldThrowException(){
+        try{
+            mate.raport(34,0);
+            fail("Nu trebuia sa ajung aici");
+        }catch (IllegalArgumentException exceptie){}
+
+    }
